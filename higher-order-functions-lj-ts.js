ca06ad1c0279eb9bd6cd306118e2d926b306d86e
NTR-TheMekaVerseclear <tamekastevenson7@gmail.com>
2022-03-28 16:44:32 -0700

    completed challenge questions 3 and 4

--- higher-order-functions-lj-ts.js
+++ higher-order-functions-lj-ts.js
@@ -37,20 +37,70 @@
 
 
 
-
-
-
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
 // var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
 // // --> "nicework"
+
+// Create a function - stringOnly
+// Use typeof to isolate the letters from the input array
+// Use .filter to return output
+// .join("") to change the array output to string output
+// input: [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+// output: "nicework"
+
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+// // --> "nicework"
+
+const stringOnly = comboArr.filter(value => {
+    return typeof value == "string"
+}) 
+
+console.log("original array: ", comboArr)
+console.log("filtered array: ", stringOnly.join(""))
+
+
+
+
+
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
 // var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
 // // --> [58, "abcd", true]
+
+
+// Create a function - mixedData
+// .filter the array input by specified values
+// input: [58, " ", "abcd", true, null, false, 0]
+// output: [58, "abcd", true]
+
+var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
+// // --> [58, "abcd", true]
+
+const mixedData = (array) => {
+    return array.filter(value => {
+        return value === true || value === "abcd" || value === 58
+    })
+}
+
+console.log(mixedData(filterArrayValues))
+
+
+
+
+
+
+
 // STRETCH Challenges
 
 // Create a function that takes in a string and returns a new string with all the vowels removed.
 // var str = "javascript is awesome"
 // // --> "jvscrpt s wsm"
+
+
+
+
+
+
+
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 // var arr1 = [3, 7, 10, 5, 4, 3]
 // var arr2 = [7, 8, 2, 1, 5, 4]
