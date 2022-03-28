d9a49384b8b6f0127be2329dac3cbf4cd86a2c6c
aclassiter <a.class.wa@gmail.com>
2022-03-28 15:21:59 -0700

    completed challenge

--- func-loops-arrays-earl-amanda.js
+++ func-loops-arrays-earl-amanda.js
@@ -1,29 +1,29 @@
 // Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
-//  var testArr1 = [3, 9, 15, 4, 10]
-//  const mult = (array) => {
-//      let newArray = []
-//      for(let i = 0; i < array.length; i++){
-//          newArray.push(array[i]*3)
-//      }
-//      return newArray
-//  }
-//  console.log(mult(testArr1))
-// // --> [9, 27, 45, 12, 30]
+ var testArr1 = [3, 9, 15, 4, 10]
+ const mult = (array) => {
+     let newArray = []
+     for(let i = 0; i < array.length; i++){
+         newArray.push(array[i]*3)
+     }
+     return newArray
+ }
+ console.log(mult(testArr1))
+// --> [9, 27, 45, 12, 30]
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
-//  var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
-//
-// const onlyOdd = (array) => {
-//   let newArr = []
-//   for(let i=0; i<array.length; i++){
-//     if(array[i] % 2 !== 0){
-//       newArr.push(array[i])
-//     }
-//   }
-//   return newArr
-// }
-//
-// console.log(onlyOdd(testArr2))
+ var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+
+const onlyOdd = (array) => {
+  let newArr = []
+  for(let i=0; i<array.length; i++){
+    if(array[i] % 2 !== 0){
+      newArr.push(array[i])
+    }
+  }
+  return newArr
+}
+
+console.log(onlyOdd(testArr2))
 
 // // --> [-7, 3, 5, 13]
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
@@ -36,16 +36,35 @@
        letterArray.push(array[i])
      }
    }
-   return letterArray
+   return letterArray.join("")
  }
  console.log(letterOnly(comboArr));
 // // --> "nicework"
 // Create a function that takes in an array of numbers and returns the sum.
-// var addThese1 = [1, 2, 3, 4]
+ var addThese1 = [1, 2, 3, 4]
+const addNum = (array) => {
+    let sum = 0
+    for(let i = 0; i < array.length; i++){
+        sum += array[i]
+      }
+    return sum 
+ }
+ console.log(addNum(addThese1))
+
 // // --> 10
 
-// var addThese2 = []
+var addThese2 = []
 // // --> 0
 // Create a function that takes in an array of numbers and returns the index of the largest number.
-// var indexHighestNumber = [1, 4, 2, 3]
-// // --> 1
+ var indexHighestNumber = [1, 4, 2, 3]
+ const largNum = (array) =>{
+    var largestNum = 0
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > largestNum ){
+        largestNum = array[i]
+      }
+    }
+    return array.indexOf(largestNum)
+  }
+  console.log(largNum(indexHighestNumber));
+// // // --> 1
