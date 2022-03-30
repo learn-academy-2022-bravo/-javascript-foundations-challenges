05114f21faef4c6bb18e7908b9bbe1bc7769dc77
nataliadiaz2 <natdiaz2001@gmail.com>
2022-03-30 15:00:09 -0700

    Additional Questions

--- js-objects-nd-ja.js
+++ js-objects-nd-ja.js
@@ -3,7 +3,7 @@
 
 // const person = {
 //   firstName: "Arthur",
-//   lastName: "Dent", 
+//   lastName: "Dent",
 //   homePlanet: "Earth",
 //   where: function(){
 //       return "Arthur Dent is from Earth"
@@ -39,15 +39,15 @@
 // console.log(product.describeProduct())
 // Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
 
-const product = {
-    name: "chair",
-    price: 24.99,
-    tax: .07,
-    describeProduct: function(){
-    return ((this.price * this.tax) + this.price).toFixed(2)
-}
-}
-console.log(product.describeProduct())
+// const product = {
+//     name: "chair",
+//     price: 24.99,
+//     tax: .07,
+//     describeProduct: function(){
+//     return ((this.price * this.tax) + this.price).toFixed(2)
+// }
+// }
+// console.log(product.describeProduct())
 
 
 // Consider this variable:
@@ -58,9 +58,39 @@ console.log(product.describeProduct())
 //   ingredients: ["bread", "peanut butter", "banana"]
 // }
 // Write the code that accesses the ingredients property.
+
+// console.log(lunch.ingredients())
+
 // Write the code that access the third ingredient of the lunch object.
+
+// console.log(lunch.ingredients[2])
+
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+//
+// const lunch = {
+//   name: "PB and Banana",
+//   type: "sandwich",
+//   ingredients: ["bread", "peanut butter", "banana"]
+// }
+//
+// const makeLunch = () => {
+//   return `The ingredients for a ${lunch.name} ${lunch.type} are ${lunch.ingredients}`
+// }
+// console.log(makeLunch(lunch))
+
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+// const lunch = {
+//   name: "PB and Banana",
+//   type: "sandwich",
+//   ingredients: ["bread", "peanut butter", "banana"],
+//   str : function () {
+//     return `The ingredients for a ${this.name} ${this.type} are ${this.ingredients}`
+//   }
+// }
+// console.log(lunch.str())
+
+
 // Consider this variable:
 
 // const animals = [
@@ -70,9 +100,42 @@ console.log(product.describeProduct())
 //   { name: "Hank", type: "cat", age: 11 }
 // ]
 // Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+//Create a function
+
+//Expected Output: array with
+
+// const onlyCats = animals.filter(value => value.type === "cat")
+// console.log(onlyCats)
+
+
 // Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+
+// const petNames = (array) => {
+//   return array.map(obj => {
+//     return obj.name
+//   })
+// }
+// console.log(petNames(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+
+// const petAge = animals.filter(value => value.age > 10)
+// console.log(petAge)
+
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 }
+]
+const animalFacts = animals.map (animal => {
+  return `The ${animal.type} named ${animal.name} and is ${animal.age} years old`
+})
+console.log(animalFacts)
+
+
 // Consider this variable:
 
 // const author = {
