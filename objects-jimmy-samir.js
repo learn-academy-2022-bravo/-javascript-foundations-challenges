de7020d7f7b7cfe7858b5aa44fd584962323ad23
Velasqj8 <jv_619@yahoo.com>
2022-03-30 15:18:57 -0700

    2nd commit

--- objects-jimmy-samir.js
+++ objects-jimmy-samir.js
@@ -28,11 +28,46 @@
 //    console.log(product.describeProduct())
 // Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
 
-const product = {
-  name: "chair",
-  price: 24.99,
-  totalWithTax: function() {
-    return `${this.price}`
+// const product = {
+//   name: "chair",
+//   price: 24.99,
+//   totalWithTax: function() {
+//     let totalPrice = (this.price * 1.07).toFixed(2)
+//     let finalTotalPrice = parseFloat(totalPrice)
+//     return finalTotalPrice
+//   }
+// }
+//
+// console.log(product.totalWithTax())
+//
+const lunch = {
+  name: "PB and Banana",
+  type: "sandwich",
+  ingredients: ["bread", "peanut butter", "banana"]
+  sandwichStuff: function() {
+    return "The ingredients for a " + this.name +
+    " " + this.type + " are " + this.ingredients
   }
 }
-console.log(totalWithTax())
+console.log(lunch.sandwichStuff())
+// Write the code that accesses the ingredients property.
+//  console.log(lunch.ingredients)
+// Write the code that access the third ingredient of the lunch object.
+//  console.log((lunch).ingredients[2])
+// Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+
+// Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+// const animals = [
+//   { name: "Waffles", type: "dog", age: 7 },
+//   { name: "Fluffy", type: "cat", age: 14 },
+//   { name: "Spelunky", type: "dog", age: 4 },
+//   { name: "Hank", type: "cat", age: 11 }
+// ]
+// const animalsNames =(array) => {
+//   return array.map(obj => {
+//     return obj.name
+//   })
+// }
+// console.log(animalsNames(animals))
