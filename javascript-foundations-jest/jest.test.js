e61052c8872a2751ff61d704fd33324cd1401393
ecostrong <necoarmstrong@mac.com>
2022-03-30 12:01:16 -0700

    initial commit

---
+++ javascript-foundations-jest/jest.test.js
@@ -0,0 +1,38 @@
+// describe("isThatsoup",( ) => {
+// it("logs 'that is soup' based on yes or no input", () => {
+//   expect(isThatsoup)("yes");toEqual("that is soup")
+//   expect(isThatsoup)("no");toEqual("that is soup")
+//     })
+// })
+//
+// //yes I failed
+//  //x logs "that is soup' based on yes or no input
+// //ReferenceError: isThatsoup is not defined
+//
+// //Green
+// //**Pseudocode***
+// //function name isThatSoup
+// //input-datatype Boolean value
+// //output-datatype string
+// //methods to be used- conditionsal
+// const isThatSoup = (string) => {
+// 	if(string === yes)
+// 	     return "that is soup"
+// 	}  else {
+// 		return "that is not soup"
+//     }
+// }
+
+describe("isThatsoup",( ) => {
+it(“takes in two arrays and add the second one to the end of the first array.
+  Then place it in alphabetical order.”’, () => {
+  expect(lotsOfSoups)(soup, bowl)) ;toEqual("that is soup")
+  expect(lotsofSoup)("no");toEqual("that is soup")
+    })
+})
+
+
+
+
+const lotOfSoup = (array1, array2) => {
+Return array1.concat(array2.).sort()
