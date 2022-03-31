37f20d935176214395872c18cac8c94419ee572e
Darwin Rivera <dgrs1995@yahoo.com>
2022-03-30 23:37:06 -0400

    initial commit

--- objects-dr-vm.js
+++ objects-dr-vm.js
@@ -87,5 +87,138 @@ const lunch = {
 //   }
 
 let bananaSandwich = (lunch) => {
-        return `The ingredients for a PB and Banana sandwich are  ${lunch.ingedients}`}
+        return `The ingredients for a PB and Banana sandwich are ${lunch.ingredients[0]}, ${lunch.ingredients[1]}, ${lunch.ingredients[2]}`
+}
 console.log(bananaSandwich(lunch))
+
+
+// Consider this variable:
+
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 }
+]
+// Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
+// const justCats = (array) =>{
+//   return array.filter(obj => 
+//     obj.type === "cat"
+//   )
+// }
+// console.log(justCats(animals))
+
+// Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+
+const justNames = (array) =>{
+  return array.map(obj => 
+    obj.name 
+  )
+}
+console.log(justNames(animals))
+
+
+
+// Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+
+
+
+const justOld = (array) =>{
+  newArray = array.filter(obj => obj.age > 10)
+  return newArray.map(obj => obj.name)
+}
+
+console.log(justOld(animals))
+
+
+// Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+
+
+// const animalIntro = (array) =>{
+//   //return `This is ${animals.name} they are a ${animals.type} and are  ${animals.age} years old`
+//   return array.map(obj =>
+//      `My name is ${obj.name} I am a ${obj.type} I am ${obj.age}`
+
+//   )
+// }
+
+//stuck ?
+
+// const justIntro = (array) =>{
+//   newArray = array.filter(obj => obj.name)
+//   return `My name is ${obj.name} I am a ${obj.type} I am ${obj.age}`
+// }
+
+// console.log(justIntro(animals))
+
+
+
+
+
+
+
+
+
+
+ 
+// console.log(animalIntro(animals))
+
+console.log("----------------------------------------------------")
+
+
+const author = {
+  name: "H. G. Wells",
+  genre: "science fiction"
+}
+// Write the code that destructures the author object so that the following code snippet will run successfully: 
+
+console.log(`${author.name} is a ${author.genre} author`)
+
+
+// Output: "H. G. Wells is a science fiction author"
+// Consider this variable:
+
+const pokeOne = {
+  species: "Charmandar",
+  pokemon_type: "Fire"
+}
+
+const pokeTwo = {
+  species: "Magikarp",
+  pokemon_type: "Water"
+}
+
+
+// Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
+
+// const describePokemon = (array) =>{
+//   return array.map(obj =>
+//     `${obj.species} is a ${obj.pokemon_type}`)
+
+// }
+
+// console.log(describePokemon(pokeOne))
+// // Output: "Charmandar is a Fire pokemon"
+// console.log(describePokemon(pokeTwo))
+// // Output: "Magikarp is a Water pokemon"
+// Consider this variable:
+
+// const triangleDimensions = {
+//   base: 2,
+//   height: 5
+// }
+// Modify the triangleDimensions object to have a method that returns the area of the triangle.
+// Write the code that will update the base to be the value of 6.
+// Consider this variable:
+
+// const learn = {
+//   cohorts: {
+//     "2021": ["Alpha", "Bravo", "Charlie", "Delta", "Echo"],
+//     "2022": ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"]
+//   }
+// }
+// Write the code that logs the name of your cohort.
+// Write the code that uses destructuring to log the name of your cohort.
+// Create a function that takes an object like the one above and returns an array with a string of every cohort name and year.
+// // Output: ["2021 Alpha", "2021 Bravo", "2021 Charlie", "2021 Delta", "2021 Echo", "2022 Alpha", "2022 Bravo", "2022 Charl
