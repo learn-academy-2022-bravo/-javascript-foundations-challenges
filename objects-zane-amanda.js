81c9b76ce6ad50c52a626609136b68a588265ff1
aclassiter <a.class.wa@gmail.com>
2022-03-31 09:20:08 -0700

    Second commit

--- objects-zane-amanda.js
+++ objects-zane-amanda.js
@@ -45,3 +45,60 @@
 //     }
 //   }
 //   console.log(product.totalWithTax())
+
+//   Write the code that accesses the ingredients property.
+// const lunch = {
+//     name: "PB and Banana",
+//     type: "sandwich",
+//     ingredients: ["bread", "peanut butter", "banana"]
+// }
+
+//console.log(lunch.ingredients)
+//   Write the code that access the third ingredient of the lunch object.
+
+//console.log(lunch.ingredients[2])
+//   Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+// const lunch = {
+//     name: "PB and Banana",
+//     type: "sandwich",
+//     ingredients: ["bread", "peanut butter", "banana"],
+//     getData: function() {
+//      return  `The ingredients for a ${this.name} sandwich are ${this.ingredients[0]}, ${this.ingredients[1]}, ${this.ingredients[2]}.`
+//     }
+// }
+//     console.log(lunch.getData())
+
+ const animals = [
+    { name: "Waffles", type: "dog", age: 7 },
+    { name: "Fluffy", type: "cat", age: 14 },
+    { name: "Spelunky", type: "dog", age: 4 },
+    { name: "Hank", type: "cat", age: 11 }
+    ]
+//   Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+// const type = animals.filter(value => value.type === "cat")
+// console.log(type)
+
+//   Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+// const namesOnly = (array) => {
+//     return array.map(obj => {
+//         return obj.name
+//     })
+// }
+// console.log(namesOnly(animals))
+
+
+//   Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+
+//Function takes in array of animal objects 
+
+//Returning array of animal names
+const overTen = (array) => {
+//Run filter on the array that is being passed to function 
+    overTenArray = array.filter(value => value.age > 10)
+    return overTenArray.map(value => value.name)
+}
+console.log(overTen(animals))
+
+
+//   Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
