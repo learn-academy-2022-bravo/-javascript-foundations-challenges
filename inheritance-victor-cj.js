8c6acb3f65be71bf96f58c12679516b299c1d85e
cjpriedel <cjpriedel@gmail.com>
2022-04-01 12:02:57 -0700

    completed Inheritance questions

--- inheritance-victor-cj.js
+++ inheritance-victor-cj.js
@@ -3,12 +3,32 @@ class Car{
     constructor(model, year){
         this.model = model
         this.year = year
+        this.lights = false
+        this.signals = false
+        this.speed = 0
+        this.acceleration = 0
+        this.braking = 0
     }
     // wheel(){
     //     return this.wheels = 4 + " wheels"
     // }
     wheel2(number){
         this.wheels = number
+    }
+    light(){
+        this.lights = !this.lights
+    }
+    signal(){
+        this.signals = !this.signals
+    }
+    currentSpeed(speedNum){
+        this.speed = speedNum
+    }
+    currentAcceleration(accNum){
+        this.acceleration = accNum
+    }
+    currentBraking(brakingNum){
+        this.braking = brakingNum
     }
 }
 
@@ -43,37 +63,89 @@ console.log(myCar)//Car { model: 'generic car', year: 1999, wheels: 4 }
 // 4. **Story**:	As a programmer, I can make a Tesla car.
 // - class Tesla inherits from class Car
 // - Create an object called myTesla which is a instance of class Tesla
-class MyTesla extends Car{
+class Tesla extends Car{
     constructor(model, year){
         super(model, year)
     }
 }
 
+var myTesla = new Tesla("Model Y", 2022)
+console.log(myTesla)
+// Tesla { model: 'Model Y', year: undefined }
+
+
 // 5. **Story**: As a programmer, I can give my Tesla a model on initialization.
 // - The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
 
+//myTesla.carModel("Model Y") //= new Tesla("Model Y", 2022)
+
+console.log(myTesla)//Tesla { model: 'Model Y', year: 2022 }
+
+
+
 // 6. **Story**: As a programmer, I can give my Tesla a year on initialization.
 // - The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+
+console.log(myTesla)//Tesla { model: 'Model Y', year: 2022 }
 
 // 7. **Story**:	As a programmer, I can make a Toyota car.
 // - class Toyota inherits from class Car
 // - create an object called myToyota which is a instance of class Toyota
 
+
+class Toyota extends Car {
+    constructor(model, year){
+        super(model, year)
+    }
+}
+
+var myToyota = new Toyota
+
+console.log(myToyota)
+
 // 8. **Story**: As a programmer, I can give my Toyota a model on initialization.
 // - The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
 
+
+var myToyota = new Toyota ("Camry", )
+
+console.log(myToyota)
+
 // 9. **Story**: As a programmer, I can give my Toyota a year on initialization.
 // - The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+
+
+var myToyota = new Toyota ("Camry", 1999)
+
+console.log(myToyota)
 
 // 10. **Story**:	As a programmer, I can make a Volkswagen car.
 // - class Volkswagen inherits from class Car
 // - create an object called myVolkswagen which is a instance of class Volkswagen
 
+
+class Volks extends Car {
+    constructor(model, year){
+        super(model, year)
+    }
+}
+
+var myVw = new Volks
+
+console.log(myVw)
+
 // 11. **Story**: As a programmer, I can give my Volkswagen a model on initialization.
 // - The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
 
+var myVw = new Volks("Jetta", )
+console.log(myVw)
+
 // 12. **Story**: As a programmer, I can give my Volkswagen a year on initialization.
 // - The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+
+
+var myVw = new Volks("Jetta", 2015)
+console.log(myVw)
 
 // 13. **Story**: As a programmer, I can give all my cars a lights property. Lights start in the off position.
 
@@ -84,16 +156,39 @@ class MyTesla extends Car{
 // 16. **Story**:	As a programmer, I can determine the speed of a car. Speed starts at 0 mph.
 
 // 17. **Story**:	As a programmer, I can speed my Tesla up by 10 per acceleration.
+myTesla.currentAcceleration(10)
+console.log(myTesla)
 
 // 18. **Story**:	As a programmer, I can slow my Tesla down by 7 per braking.
 
-// 19. **Story**:	As a programmer, I can speed my Toyota up by 5 per acceleration.
+myTesla.currentBraking(7)
+console.log(myTesla)
+
+// 19. **Story**:	As a programmer, I can speed my Toyota up by 5 per acceleration. 
+
+myToyota.currentAcceleration(5)
+console.log(myToyota)
 
 // 20. **Story**:	As a programmer, I can slow my Toyota down by 2 per braking.
 
+myToyota.currentBraking(2)
+console.log(myToyota)
+
+
 // 21. **Story**:	As a programmer, I can speed my Volkswagen up by 7 per acceleration.
+
+myVw.currentAcceleration(7)
+console.log(myVw)
 
 // 22. **Story**:	As a programmer, I can slow my Volkswagen down by 5 per braking.
 
+myVw.currentBraking(5)
+console.log(myVw)
+
+
 // 23. **Story**:  As a programmer, I can call upon a carInfo method that will tell me all the information about a car.
+
+//Done
+
 // - The method can be created in the parent class and accessed by all child classes
+// Done 
